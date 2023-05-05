4f99bdbc055da854f86e0078b41e1b231494a66b
Vidhyasagar Harihara <vidhyasagar1210@gmail.com>
2023-05-04 22:05:50 -0400

    feat: Add MeetupReminder event

---
+++ src/main/scala/io/fitcentive/registry/events/meetup/MeetupReminder.scala
@@ -0,0 +1,18 @@
+package io.fitcentive.registry.events.meetup
+
+import com.google.pubsub.v1.PubsubMessage
+import io.circe.Codec
+import io.circe.generic.semiauto.deriveCodec
+import io.fitcentive.sdk.gcp.pubsub.PubSubMessageConverter
+import io.fitcentive.sdk.utils.PubSubOps
+
+import java.util.UUID
+
+case class MeetupReminder(meetupId: UUID, meetupName: String, targetUser: UUID) {}
+
+object MeetupReminder extends PubSubOps {
+  implicit val codec: Codec[MeetupReminder] = deriveCodec[MeetupReminder]
+
+  implicit val converter: PubSubMessageConverter[MeetupReminder] =
+    (message: PubsubMessage) => message.decodeUnsafe[MeetupReminder]
+}
