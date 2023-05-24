73927601e9e56e0382df41e97221e5a6fd8a0d76
Vidhyasagar Harihara <vidhyasagar1210@gmail.com>
2023-05-24 17:41:33 -0400

    feat: Add ScheduleMeetupStateTransitionTimeForLater

---
+++ src/main/scala/io/fitcentive/registry/events/scheduled/ScheduleMeetupStateTransitionTimeForLater.scala
@@ -0,0 +1,19 @@
+package io.fitcentive.registry.events.scheduled
+
+import com.google.pubsub.v1.PubsubMessage
+import io.circe.Codec
+import io.circe.generic.semiauto.deriveCodec
+import io.fitcentive.sdk.gcp.pubsub.PubSubMessageConverter
+import io.fitcentive.sdk.utils.PubSubOps
+
+import java.util.UUID
+
+case class ScheduleMeetupStateTransitionTimeForLater(meetupId: UUID, scheduleTransitionAtMillis: Long)
+
+object ScheduleMeetupStateTransitionTimeForLater extends PubSubOps {
+  implicit val codec: Codec[ScheduleMeetupStateTransitionTimeForLater] =
+    deriveCodec[ScheduleMeetupStateTransitionTimeForLater]
+
+  implicit val converter: PubSubMessageConverter[ScheduleMeetupStateTransitionTimeForLater] =
+    (message: PubsubMessage) => message.decodeUnsafe[ScheduleMeetupStateTransitionTimeForLater]
+}
